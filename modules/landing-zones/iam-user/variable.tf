bfdb597f89f9e0d68f4336bb2471d1182a1676a9
Shuntaro Nakano <nshun583@gmail.com>
2024-01-15 00:42:19 +0900

    added CloudWatch feature to iam module

--- modules/landing-zones/iam-user/variable.tf
+++ modules/landing-zones/iam-user/variable.tf
@@ -2,3 +2,8 @@ variable "user_name" {
   description = "The user name to use"
   type        = string
 }
+
+variable "give_neo_cloudwatch_full_access" {
+  description = "If true, neo gets full access to CloudWatch"
+  type        = bool
+}
